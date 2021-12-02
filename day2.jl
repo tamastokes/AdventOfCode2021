12559a3626181fc126b453891f250606241e14d5
Tamás Tőkés <tamastokes@gmail.com>
2021-12-02 14:16:01 +0100

    Save unsaved changes

--- day2.jl
+++ day2.jl
@@ -22,3 +22,24 @@ function calcpos(filename)
     end
     return horizontalPos * depth
 end
+
+function calcpos2(filename)
+    lines = readlines(filename)
+    coursePlan = getCourseFromLine.(lines)
+    horizontalPos = 0
+    depth = 0
+    aim = 0
+    for step in coursePlan
+        if step[1] == "forward"
+            horizontalPos += step[2]
+            depth += (step[2]*aim)
+        elseif step[1] == "up"
+            aim -= step[2]
+        elseif step[1] == "down"
+            aim += step[2]
+        else
+            throw(AssertionError(step))
+        end
+    end
+    return horizontalPos * depth
+end
