97ef223f9f486bf2211119e96a0ae8f54f4a1e46
Tamás Tőkés <tamastokes@gmail.com>
2021-12-05 20:26:05 +0100

    Solve part 1 of day 5

---
+++ day5.jl
@@ -0,0 +1,52 @@
+function getLinesFromInput(input)
+    lines = zeros(Int32, 0)
+    splitInput = split.(input, " ")
+    for i in splitInput
+        (x1, y1) = parse.(Int32, split(i[1],","))
+        (x2, y2) = parse.(Int32, split(i[3],","))
+        append!(lines, [x1, y1, x2, y2,]) 
+    end
+    return transpose(reshape(lines, 4, :))
+end
+
+function processInput(filename)
+    input = readlines(filename)
+    return getLinesFromInput(input)
+end
+
+function getSequence(n1, n2)
+    return min(n1,n2):max(n1,n2)
+end
+
+function getPointsInLine(line)
+    points = zeros(typeof(l[1]),0)
+    if line[1] == line[3]
+        for i in getSequence(line[2],line[4])
+            append!(points, line[1])
+            append!(points, i)
+        end
+    elseif line[2] == line[4]
+        for i in getSequence(line[1],line[3])
+            append!(points, i)
+            append!(points, line[2])
+        end
+    end
+    return transpose(reshape(points, 2, :))
+end
+
+function fillGrid(lines)
+    size = convert(Int64,maximum(lines)) + 1
+    grid = reshape(zeros(typeof(lines[1]), size^2), size, :)
+    for i in 1:length(lines[:,1])
+        points = getPointsInLine(lines[i,:])
+        for point in eachrow(points)
+            grid[point[1]+1,point[2]+1] += 1
+        end
+    end
+    return grid
+end
+
+function findOverlaps(filename)
+    lines = processInput(filename)
+    return count(x->x>1,fillGrid(lines))
+end
